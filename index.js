579affaa53a648987f83b966a48a2d57b85cd75c
francisco figueiredo <figueiredofb66@gmail.com>
2021-09-03 01:19:53 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,54 @@
+{nome : 'Sport X Nautico'
+nomeDoEvento = 'Sport x Nautico';
+qtdIngresso = 10000;
+idadePermitida = 18;
+diaDoEvento = 17;
+mesDoEvento = 01;}
+
+//Lista de jogadores
+let jogador1 = {
+    nome : 'Jean Carlos',
+    idade : 25,
+    diaDeInscricao : 16,
+    mesDeInscricao : 01
+}
+
+let jogador2 = {
+    nome : 'Kieza matador de Leao',
+    idade : 17,
+    diaDeInscricao : 15,
+    mesDeInscricao : 01
+}
+
+let jogador3 = {
+    nome : 'Thiago branca de Neves',
+    idade : 20,
+    diaDeInscricao : 01,
+    mesDeInscricao : 01
+}
+
+let jogador4 = {
+    nome : 'Magrão mao de lodo',
+    idade : 24,
+    diaDeInscricao : 02,
+    mesDeInscricao : 01
+}
+if (jogador1.idade < 18 ){
+    console.log(jogador1.nome  + 'erro - motivo:juvenil')
+}
+if (jogador2.idade < 18){
+    console.log(jogador2.nome + 'erro - motivo: juvenil')
+
+}else{ console.log('bem vindo capita')
+}
+    if (jogador1.diaDeInscricao > 17 && mesDeInscricao > mesDoEvento){
+        console.log(nome + ' barrado - motivo: fim do periodo de inscricao')
+
+    }else{
+        console.log('inscrito com sucesso!')
+    }
+    for (let index = 0; index < jogadores.length; index++){
+        if(array [index].idade < 18){
+
+        }
+    }
